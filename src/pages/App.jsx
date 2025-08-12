6a3acfd1cac62edb496fad73b197d1f5b11326a6
mateus celestno <mateusnascimentocelestino@gmail.com>
2025-08-11 21:01:25 -0300

    add data aos m14

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
 import Navbar from "../components/NavBar";
 import Home from "./Home.jsx";
 import Precheckout from "./Prechekout.jsx";
 import AdminLogin from "./Adminlogin.jsx";
 import AdminDashboard from "./Admin.jsx";
 import PrivateRoute from "../components/PrivateRouter.jsx";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
+  
+  useEffect(() => {
+     AOS.init({
+       duration: 1000, // duração da animação (ms)
+       once: true,     // anima apenas uma vez
+     });
+   }, []);
+
+  
   return (
     <div className="m-w-[100vw] w-[100%] relative">
       <Navbar />
